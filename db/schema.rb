f5307cefd9e81a064ddc37bf43c17a43ad7195e8
Pedro <priccis@gmail.com>
2019-04-12 18:54:13 -0400

    schema from first migration

---
+++ db/schema.rb
@@ -0,0 +1,163 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_04_12_223546) do
+
+  create_table "admin_geofences", force: :cascade do |t|
+    t.integer "user_id"
+    t.decimal "latitude", precision: 8, scale: 6
+    t.decimal "longitude", precision: 9, scale: 6
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_admin_geofences_on_user_id"
+  end
+
+  create_table "blacklists", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_blacklists_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "dumpsters", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "creator_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["creator_id"], name: "index_dumpsters_on_creator_id"
+    t.index ["post_id"], name: "index_dumpsters_on_post_id"
+  end
+
+  create_table "file_types", force: :cascade do |t|
+    t.string "name"
+    t.string "image_path"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "follow_posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_follow_posts_on_post_id"
+    t.index ["user_id"], name: "index_follow_posts_on_user_id"
+  end
+
+  create_table "follow_users", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "followed_user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_user_id"], name: "index_follow_users_on_followed_user_id"
+    t.index ["user_id"], name: "index_follow_users_on_user_id"
+  end
+
+  create_table "post_flags", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "flagged_by_id"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flagged_by_id"], name: "index_post_flags_on_flagged_by_id"
+    t.index ["post_id"], name: "index_post_flags_on_post_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "description"
+    t.boolean "is_resolved"
+    t.text "content"
+    t.decimal "location_latitude", precision: 8, scale: 6
+    t.decimal "location_longitude", precision: 9, scale: 6
+    t.string "city"
+    t.string "country"
+    t.boolean "is_open"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "shared_files", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.integer "file_type_id"
+    t.string "path"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["file_type_id"], name: "index_shared_files_on_file_type_id"
+    t.index ["post_id"], name: "index_shared_files_on_post_id"
+    t.index ["user_id"], name: "index_shared_files_on_user_id"
+  end
+
+  create_table "user_profiles", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_profiles_on_user_id"
+  end
+
+  create_table "user_shareds", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_user_shareds_on_post_id"
+    t.index ["user_id"], name: "index_user_shareds_on_user_id"
+  end
+
+  create_table "user_statuses", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password"
+    t.boolean "is_admin"
+    t.boolean "is_superadmin"
+    t.integer "user_status_id"
+    t.datetime "last_access"
+    t.integer "karma"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_status_id"], name: "index_users_on_user_status_id"
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.integer "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_votes_on_post_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
+end
