0265d25ae5b3f0c003884aefa88bbf730322dbbc
Pedro <priccis@gmail.com>
2019-04-12 21:27:37 -0400

    Fix follow_user relationship association v2

--- db/migrate/20190412222810_create_follow_users.rb
+++ db/migrate/20190412222810_create_follow_users.rb
@@ -1,13 +1,13 @@
 class CreateFollowUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :follow_users do |t|
-      t.references :user
+      t.references :follower_user
       t.references :followed_user
 
       t.timestamps
     end
-    add_index :follow_users, :user
+    add_index :follow_users, :follower_user
     add_index :follow_users, :followed_user
-    add_index :follow_users, [:user, :followed_user], unique: true
+    add_index :follow_users, [:follower_user, :followed_user], unique: true
   end
 end
