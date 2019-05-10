977d7c54b4d63f243aa2a2c54cc982ad352e287b
Pedro <priccis@gmail.com>
2019-05-10 16:16:11 -0400

    Signup view

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,6 @@ Rails.application.routes.draw do
   resources :comments
   resources :posts
   resources :users
+  get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
