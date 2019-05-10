db0dd400b58e0cfc3668f57e19632f70711b02e5
Pedro <priccis@gmail.com>
2019-05-10 18:45:29 -0400

    New blank Admin controller to contain all related to admin work

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
   resources :posts
   resources :users
   get '/signup', to: 'users#new'
+  get '/admin', to: 'admin#page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
