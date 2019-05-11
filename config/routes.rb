1f3b058591df0121cf662a60d068b5200e88559e
Pedro <priccis@gmail.com>
2019-05-10 22:03:34 -0400

    Log in (hard) view

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :users
   get '/signup', to: 'users#new'
+  get '/login', to: 'welcome#login' # TODO: to sessions#new
   get '/admin', to: 'admin#page'
   get '/admin/blacklist', to: 'admin#blacklist'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
