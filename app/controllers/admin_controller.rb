32c068358e5ad1190c7223c8fab17c6e650346ed
Pedro <priccis@gmail.com>
2019-05-10 21:43:05 -0400

    Admin blacklist

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,7 @@
 class AdminController < ApplicationController
   def page
   end
+
+  def blacklist
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,6 @@ Rails.application.routes.draw do
   resources :users
   get '/signup', to: 'users#new'
   get '/admin', to: 'admin#page'
+  get '/admin/blacklist', to: 'admin#blacklist'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
