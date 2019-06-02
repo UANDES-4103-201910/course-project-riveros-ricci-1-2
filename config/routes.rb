e382d9fef965be6869f57dce7be2363d4d12d107
Pedro <priccis@gmail.com>
2019-06-02 14:23:07 -0400

    Refactor routes
    Comments belong to Posts.
    Dumpster and Blcklist belong to Admin.

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,10 @@ Rails.application.routes.draw do
   root 'posts#index'
   # get '/post', to: 'welcome#post' # Temporal, in future specify post id
 
-  get '/post/create_post', to: 'posts#create_post'
   resources :post_flags
-  resources :admin_geofences
   resources :user_profiles
   get 'profile', to: 'users#show'
   resources :follow_users
-  resources :dumpsters
-  resources :blacklists
   resources :votes
   resources :shared_files
   resources :file_types
@@ -22,12 +18,17 @@ Rails.application.routes.draw do
   resources :user_statuses
   resources :follow_posts
   resources :comments
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   resources :users
   get '/signup', to: 'users#new'
   get '/login', to: 'welcome#login' # TODO: to sessions#new
+  scope '/admin' do
+    resources :admin_geofences, :blacklists, :dumpsters
+  end
   get '/admin', to: 'admin#page'
-  get '/admin/blacklist', to: 'admin#blacklist'
-  get '/admin/dumpster', to: 'dumpsters#index'
+  # get '/admin/blacklist', to: 'admin#blacklist'
+  # get '/admin/dumpster', to: 'dumpsters#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
