7cc93c415d2c3eae31441a18524d8973d474f21c
Pedro <priccis@gmail.com>
2019-06-02 22:20:08 -0400

    Remove hack in users's controller
    Was already implemented correctly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,6 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-    @user.user_status_id = 1 # TODO: un-hardcode
 
     respond_to do |format|
       if @user.save
