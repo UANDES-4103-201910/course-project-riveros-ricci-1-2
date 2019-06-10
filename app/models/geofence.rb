821922fcb5505f60d120cb1d1add3d90c8aa48af
Pedro <priccis@gmail.com>
2019-06-09 23:12:02 -0400

    Add method to get if point is contained in geofence
    Using gem GeoRuby

--- app/models/geofence.rb
+++ app/models/geofence.rb
@@ -1,6 +1,19 @@
 class Geofence < ApplicationRecord
+  include GeoRuby::SimpleFeatures
+
   has_many :geofence_vertices, dependent: :destroy
   has_and_belongs_to_many :users
 
   attr_accessor :points
+
+  def build_polygon
+    points = []
+    self.geofence_vertices.map { |v| points << [v.latitude, v.longitude] }
+    Polygon.from_coordinates([points], 4326)
+  end
+
+  def contains_location?(lat, lng)
+    point = Point.from_x_y(lat, lng)
+    build_polygon.contains_point?(point)
+  end
 end
